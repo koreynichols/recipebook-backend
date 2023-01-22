753b527457a90342c81fa8feab09a21c5761fbad
Korey Nichols <koreynichols1@gmail.com>
2023-01-21 16:55:50 -0800

    working on models

--- recipebook/recipe/models.py
+++ recipebook/recipe/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Recipe(models.Model):
-    dish = models.TextField()
+    dish_name = models.TextField()
+    prep_time = models.TextField()
+    cook_time = models.TextField
     ingredients = models.TextField()
+    steps = models.TextField()
